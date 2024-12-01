84bc0f94ece9c3d8fe10a22b033f842fe09b066a
liqi6 <s1143012@gm.ncue.edu.tw>
2024-12-01 14:15:15 +0800

    Update 7_homework.py

--- pages/7_homework.py
+++ pages/7_homework.py
@@ -1,7 +1,6 @@
 import leafmap
 
-m = leafmap.Map(center=(22.439, 120.538), zoom=14, height="600px") 
-m.split_map(
-    left_layer="HYBRID", right_layer="ESA WorldCover 2020"
-)
-m.add_legend(title="ESA Land Cover", builtin_legend="ESA_WorldCover")
+m = leafmap.Map()
+url = "https://github.com/opengeos/datasets/releases/download/world/world_cities.csv"
+m.add_marker_cluster(url, x="longitude", y="latitude", layer_name="World cities")
+m
