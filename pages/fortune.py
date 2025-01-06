6cc4f87ee83bcf703eecd5cd3f7aaf2da7a9e13e
liqi6 <s1143012@gm.ncue.edu.tw>
2025-01-06 16:12:32 +0800

    Update fortune.py

--- pages/fortune.py
+++ pages/fortune.py
@@ -1,17 +1,12 @@
 import os
 import random
 import streamlit as st
-import random
 
 # 顏色列表
 colors = ["紅色", "橙色", "黃色", "綠色", "藍色", "紫色", "粉紅色", "黑色", "白色", "灰色", "水藍色"]
 
 # 隨機選擇一個顏色
 selected_color = random.choice(colors)
-
-# 顯示隨機選擇的顏色
-print(f"今天的幸運色是: {selected_color}")
-
 
 # 顏色與特性
 colors_data = {
@@ -28,15 +23,16 @@ colors_data = {
     "水藍色": {"象徵意義": "自由、清新、希望", "感受": "清涼、純粹、輕快", "心情": "自由、舒適、放鬆"},
 }
 
-# 顯示所有顏色
-st.title("產品顏色列表")
+# 顯示標題
+st.title("今天的幸運色")
 
+# 顯示隨機選擇的顏色
+st.write(f"今天的隨機選擇顏色是: **{selected_color}**")
 
-# 當用戶選擇顏色時，顯示該顏色的詳細資料
-selected_color = st.selectbox("選擇一個顏色", list(colors_data.keys()))
+# 顯示顏色的詳細資料
 if selected_color:
     color_info = colors_data[selected_color]
     st.write(f"象徵意義: {color_info['象徵意義']}")
     st.write(f"感受: {color_info['感受']}")
     st.write(f"心情: {color_info['心情']}")
-    
+
